6725620f73f88f92af4c5dac725cce6233d790ed
davisrwe <71826302+davisrwe@users.noreply.github.com>
2021-08-03 14:24:13 +0300

    Final commit

--- Random_ForestC_IDA_MLModel.R
+++ Random_ForestC_IDA_MLModel.R
@@ -1,67 +1,34 @@
-#import packages for reading files
-install.packages("RWeka")
-library("RWeka")
+#import libraries and datasets
 
-#importing patients' datasets (heart disease, liver disorders, lung cancer, kidney disease) as dataframes
+library(randomForest)
+rfNews()
+require(caTools)
 
-heartdisease_df_cleveland = setNames(
+data = setNames(
   data.frame(
     read.table(
       "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data",sep=",")),
   c('age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','num the predicted attribute'))
 
-heartdisease_df_hungarian = setNames(
-  data.frame(
-    read.table(
-      "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.hungarian.data",sep=",")),
-  c('age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','num the predicted attribute'))
+#To check the structure of our data
+dim(data)
+names(data) <- c("age", "sex", "cp", "trestbps", "choi", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thai", "num")
+head(data)
+data$num[data$num > 1] = 1
+summary(data)
 
-heartdisease_df_switzerland = setNames(
-  data.frame(
-    read.table(
-      "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.switzerland.data",sep=",")),
-  c('age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','num the predicted attribute'))
+#To check the structure of our data
+sapply(data, class)
 
-heartdisease_df_long_beach_va = setNames(
-  data.frame(
-    read.table(
-      "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.va.data",sep=",")),
-  c('age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','num the predicted attribute'))
+#We need to change the data structures of our respective columns to reflect the nature of the data collected in the respective column
 
-liverdisorder_df = setNames(
-  data.frame(
-    read.table(
-      "https://archive.ics.uci.edu/ml/machine-learning-databases/liver-disorders/bupa.data",sep=",")),
-  c('mcv','alkphos','sgpt','sgot','gammagt','drinks drunk per day','selector'))
-
-lungcancer_df = data.frame(
-  read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/lung-cancer/lung-cancer.data",sep=","))
-
-#before running this code, please download & unzip this file -> in a folder where this script is located -> https://archive.ics.uci.edu/ml/machine-learning-databases/00336/Chronic_Kidney_Disease.rar
-kidneydiseasse_df = data.frame(
-  read.arff("chronic_kidney_disease.arff"))
-
-#Random Forest Classifier Machine Learning Algorithm
-
-install.packages("randomForest")
-install.packages("caTools")
-library(randomForest)
-library(caTools)
-
-#Data cleaning :Change 2,3,4 in num column to 1
-heartdisease_df_cleveland$`num the predicted attribute`[heartdisease_df_cleveland$`num the predicted attribute`>1]=1
-
-#Data cleaning : Transform method to change the in built type of each feature.
-summary(heartdisease_df_cleveland)
-sapply(heartdisease_df_cleveland,class)
-
-heartdisease_df_cleveland = transform(
-  heartdisease_df_cleveland,
+data <- transform(
+  data,
   age=as.integer(age),
   sex=as.factor(sex),
   cp=as.factor(cp),
   trestbps=as.integer(trestbps),
-  chol=as.integer(chol),
+  choi=as.integer(choi),
   fbs=as.factor(fbs),
   restecg=as.factor(restecg),
   thalach=as.integer(thalach),
@@ -69,14 +36,45 @@ heartdisease_df_cleveland = transform(
   oldpeak=as.numeric(oldpeak),
   slope=as.factor(slope),
   ca=as.factor(ca),
-  thal=as.factor(thal),
-  `num the predicted attribute`=as.factor(`num the predicted attribute`)
+  thai=as.factor(thai),
+  num=as.factor(num)
 )
 
-summary(heartdisease_df_cleveland)
-sapply(heartdisease_df_cleveland,class)
+#Checking to see if transformation is complete
+sapply(data, class)
 
-heartdisease_df_cleveland[ heartdisease_df_cleveland == "?"] = NA
-colSums(is.na(heartdisease_df_cleveland))
+#removing null/missing values from the dataset
+data[ data == "?"] = NA
+colSums(is.na(data))
 
+data$thai[which(is.na(data$thai))] <- as.factor("3.0")
+data <- data[!(data$ca %in% c(NA)),]
+colSums(is.na(data))
 
+#Refactoring ca and thai columns data after amending the null/missing values
+summary(data)
+data$ca <- factor(data$ca)
+data$thai <- factor(data$thai)
+
+#Split_the data_set into training and test set
+sample = sample.split(data$num, SplitRatio = .75)
+train = subset(data, sample == TRUE)
+test  = subset(data, sample == FALSE)
+dim(train)
+dim(test)
+
+#run (build) random forest classifier using default presets i.e. number of trees 500 & the number of features used as potential candidates for each split is 3
+rf <- randomForest(num ~ .,  data=train)
+
+#Get Prediction data values
+pred = predict(rf, newdata=test[-14])
+head(pred)
+
+#print confusion matrix and relevant metrics for model evaluation, first we import the caret package
+
+install.packages("caret")
+install.packages("e1071")
+library(e1071)
+library(caret)
+
+confusionMatrix(pred,test$num)
